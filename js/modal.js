810f8ff132407a32a5b984650883eac105a171c2
Talaibekoff <talaibekoff@yandex.ru>
2020-07-02 20:28:40 +0600

    Подключил библиотеку jQuery. Написал код для модальных окон используя библиотеку jQuery

---
+++ js/modal.js
@@ -0,0 +1,43 @@
+/* jQuery ждет (ready), когда document прогрузится
+То есть скрипт начинает работать только после того, 
+как документ полностью прогрузится */
+$(document).ready(function () {
+    var button = $('#button'); //Объявил переменную button
+    var modal = $('#modal'); //Объявил переменную modal
+    var close = $('#close'); //Объявил переменную close
+    var buttonSuccess = $('#button-success'); //Объявил переменную buttonSuccess
+    var success = $('#success'); //Объявил переменную success
+    var closeSuccess = $('#close-success'); //Объявил переменную closeSuccess
+
+    /* При клике на button, к модальному окну 
+    присваивается класс-модификатор modal_active */
+    button.on('click', function () { // on - отслеживание действия "клик"
+        modal.addClass('modal_active'); //addClass - присвоение нового класса
+    });
+    
+    /* При клике на close, у модального окна 
+    удаляется класс-модификатор modal_active */
+    close.on('click', function () {
+        modal.removeClass('modal_active'); // removeClass - удаление класса
+    });
+
+    /* При клике на buttonSuccess у модального окна
+    удаляется класс modal_active, а к окну success 
+    присваивается класс success_active */
+    buttonSuccess.on('click', function () {
+        modal.removeClass('modal_active');
+        success.addClass('success_active');
+
+        /* После появления окна success через 5 секунд
+        автоматически удаляется класс success_active */
+        setTimeout(() => {  // Установление таймаута
+            success.removeClass('success_active');
+        }, 5000); // В милисекундах
+    });
+
+    closeSuccess.on('click', function () {
+        success.removeClass('success_active');
+    });
+
+
+});
